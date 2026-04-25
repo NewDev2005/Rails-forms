d9825fe8f9c47ef98b5acb28096e883837f0d74e
krypto <clayjeansen3@gmail.com>
2026-04-25 13:00:08 +0530

    add routes for create , edit and new

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, only: [ :new, :create ]
+  get "/users/:id", to: "users#edit"
 end
